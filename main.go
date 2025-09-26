6a84b0305f7b8c4a2ca5defb79a6d94c085b5b4d
Braian <Braianvidela324@gmail.com>
2025-09-26 14:17:36 -0300

    agrego script, estilos.css, cambios en index, eliminacion de contacto

--- main.go
+++ main.go
@@ -6,35 +6,15 @@ import (
 	/*"strings"*/)
 
 func main() {
-	// 1. Define el contenido HTML AAAAAAAAAAAAAAAAAAAAA
+	// 1. Define el contenido HTML
 	htmlContent := "./static"
 	fileServer := http.FileServer(http.Dir(htmlContent))
 	// 2. Registra un manejador (handler) para la ruta raíz "/"
 	http.Handle("/", fileServer)
 
-	// 2. Registra un manejador (handler) para la ruta raíz "/"
-	/*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		//  http.ResponseWriter Para escribir la respuesta HTTP
-		// *http.Request: Contiene información de la solicitud
-		// 3. Establece la cabecera Content-Type
-		// 4. Muestra el formulario cuando la solicitud es GET
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		fmt.Fprint(w, htmlContent)
-
-	})*/
-
-	http.HandleFunc("/contacto", contacto)
+	//http.HandleFunc("/contacto", contacto)
 
 	// 5. Define el puerto y muestra un mensajew.Header().Set("Content-Type", "text/html; charset=utf-8")
 	iniciarServidor()
 
 }
-
-// 1. Parsear los datos del formulario (¡Crucial!) //resuelto con FORMVALUE
-/*if err := r.ParseForm(); err != nil {
-	http.Error(w, "Error al parsear", http.StatusBadRequest)
-	return
-}*/
-
-//para mostrar esta informacion del servidor debo abrir otra terminal
-//curl -v http://localhost:8080
